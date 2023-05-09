ca3d8db819a8d2d783cf9ffc2a1f8a3128a72a97
naser2200 <naser.shabani@gmail.com>
2023-05-09 19:17:33 +0200

    language counting

--- language.py
+++ language.py
@@ -12,8 +12,11 @@ headers = {
 # Calculate the date one year ago
 one_year_ago = datetime.now() - timedelta(days=365)
 
+# Create a dictionary to count the occurrences of each language
+language_counts = {}
+
 # Loop over each day in the last year
-for day in range(365):
+for day in range(1):  # We're now only looping over one day to reduce the number of requests
     # Calculate the start and end dates for this day
     start_date = one_year_ago + timedelta(days=day)
     end_date = start_date + timedelta(days=1)
@@ -23,7 +26,7 @@ for day in range(365):
     end_date_str = end_date.isoformat()
 
     # The GitHub API url for searching repositories
-    url = f'https://api.github.com/search/repositories?q=created:{start_date_str}..{end_date_str}'
+    url = f'https://api.github.com/search/repositories?q=created:{start_date_str}..{end_date_str}&per_page=100'
 
     response = requests.get(url, headers=headers)
 
@@ -31,22 +34,29 @@ for day in range(365):
     if response.status_code != 200:
         print(f"Request failed with status code {response.status_code}")
     else:
-        # Check rate limit info
-        rate_limit_info = response.headers
-        print('Limit: ', rate_limit_info['X-RateLimit-Limit'])
-        print('Remaining: ', rate_limit_info['X-RateLimit-Remaining'])
-        print('Reset: ', rate_limit_info['X-RateLimit-Reset'])
-        
         # The response will be a JSON object containing the search results
         data = response.json()
 
         # Check if 'items' key exists in the response data
         if 'items' in data:
-            # You can now access the data in the response. For example, to print the names of all the repositories:
+            # Loop over each repository in the results
             for item in data['items']:
-                print(item['name'])
+                # Get the repository's primary language
+                language = item['language']
+                
+                # If the language is not None, increment its count in the dictionary
+                if language is not None:
+                    if language in language_counts:
+                        language_counts[language] += 1
+                    else:
+                        language_counts[language] = 1
         else:
             print("No 'items' key in the response. Response data: ", data)
 
     # Sleep for a short time to avoid hitting the rate limit
     time.sleep(2)
+
+# After all requests are done, print the top 10 languages
+top_languages = sorted(language_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+for language, count in top_languages:
+    print(f'{language}: {count} repositories')
